bdca393d2196473301776d4f43ca5b5c00ffec8d
Vikas Chauhan <vikas@rockstarchef.in>
2023-07-29 13:27:14 +0530

    out change in user route

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/user', function (req, res) {
-  res.send('This is user api first222');
+  res.send('This is user api first2224333');
 });
 
 module.exports = router;
